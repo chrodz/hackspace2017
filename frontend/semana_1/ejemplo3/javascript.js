772d6bd8039233ecc58a96578f87cbcba316a851
Pierre chavez <pierre.rodriguez@comercio.com.pe>
2017-01-23 01:11:05 -0500

    frontend

---
+++ frontend/semana_1/ejemplo3/javascript.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Chroz on 23/01/2017.
+ */
+function miFuncion() {
+    var x = document.getElementById("demo");
+    x.style.fontSize = "25px";
+    x.style.color = "red";
+    x.style.textAlign = "center";
+}
